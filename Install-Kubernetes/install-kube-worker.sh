ccc51988394f7c4661c30b85deb9af0723c58692
Taher Shaker <35845826+tahershaker@users.noreply.github.com>
2024-04-01 00:29:30 +0200

    fc

--- Install-Kubernetes/install-kube-worker.sh
+++ Install-Kubernetes/install-kube-worker.sh
@@ -15,71 +15,97 @@ set -x
 #------------------------------------------------------
 
 # Set required variables
-KUBEVERSION=1.26.10-00
+export KUBEVERSION="1.26.10-1.1"
+export KUBE_VERSION_SHORT="v1.26"
+export MASTER_PRIV_IP="10.10.10.162"
+export WORKER_01_IP="10.10.10.53"
+export BASTION_NOST_IP="10.10.1.138"
+export STAGE_MASTER_IP="10.10.13.105"
+export STAGE_WORKER_IP="10.10.13.10"
+export BASTION_NOST_FQDN="demo-a-bastion-01.rancher-demo.io"
+export BASTION_NOST_FQDN_SHORT="demo-a-bastion-01"
+export MASTER_NODE_FQDN="demo-a-mgmt-master-01.rancher-demo.io"
+export MASTER_NODE_FQDN_SHORT="demo-a-mgmt-master-01"
+export WORKER_NODE_FQDN="demo-a-mgmt-worker-01.rancher-demo.io"
+export WORKER_NODE_FQDN_SHORT="demo-a-mgmt-worker-01"
+export STAGE_MASTER_FQDN="demo-a-stage-master-01.rancher-demo.io"
+export STAGE_MASTER_FQDN_SHORT="demo-a-stage-master-01"
+export STAGE_WORKER_FQDN="demo-a-stage-worker-01.rancher-demo.io"
+export STAGE_WORKER_FQDN_SHORT="demo-a-stage-worker-01"
 
-#Install Containerd On Server
-#References: https://kubernetes.io/docs/setup/production-environment/container-runtimes/#containerd
-#   Load kernel modules and modify system settings as a prerequisites (Overlay - Netfilter - IpForwarding).
+#------------------------------------------------------
 
-cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+# Configure Hostname and /etc/hosts file
+
+# Set the Hostnames to preserve after rebooting
+sudo sed -i 's\preserve_hostname: false\preserve_hostname: true\g' /etc/cloud/cloud.cfg
+
+# Set Hostname
+sudo hostnamectl set-hostname $STAGE_WORKER_FQDN
+
+# Edit the /etc/hosts file to add the local hostname
+sudo sed -i "s\127.0.0.1 localhost\127.0.0.1  $STAGE_WORKER_FQDN $STAGE_WORKER_FQDN_SHORT\g" /etc/hosts
+# Edit the /etc/hosts file to add other hostname
+sudo cat << EOF >> /etc/hosts
+$BASTION_NOST_IP         $BASTION_NOST_FQDN $BASTION_NOST_FQDN_SHORT
+$WORKER_01_IP         $WORKER_NODE_FQDN $WORKER_NODE_FQDN_SHORT
+$MASTER_PRIV_IP         $MASTER_NODE_FQDN $MASTER_NODE_FQDN_SHORT
+$STAGE_MASTER_IP         $STAGE_MASTER_FQDN $STAGE_MASTER_FQDN_SHORT
+EOF
+
+#------------------------------------------------------
+
+# Perform Kubernetes Installation Prerequisits
+
+# Disable Swap
+sudo swapoff -a
+sudo sed -i '/ swap / s/^/#/' /etc/fstab
+
+#Set up the IPV4 bridge on all nodes
+sudo cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
 overlay
 br_netfilter
 EOF
-
 sudo modprobe overlay
 sudo modprobe br_netfilter
 
-# Setup required sysctl params, these persist across reboots.
-cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/kubernetes.conf
 net.bridge.bridge-nf-call-iptables  = 1
-net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
 EOF
 
 # Apply sysctl params without reboot
 sudo sysctl --system
 
-#   Install Containerd 
+#------------------------------------------------------
 
-sudo apt-get update && sudo apt-get install -y containerd
-sudo mkdir -p /etc/containerd
-containerd config default | sudo tee /etc/containerd/config.toml
+#Install containerd 
+sudo apt-get update && sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update && sudo apt install -y containerd.io
+
+# Configure containerd so that it starts using systemd as cgroup.
+containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+
 sudo systemctl restart containerd
+sudo systemctl enable containerd
+ 
+#------------------------------------------------------
 
-#------------------------------------------------------------
+# Installa Kubernetes
 
+# Add Apt Repository for Kubernetes
+sudo mkdir /etc/apt/keyrings
+sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/$KUBE_VERSION_SHORT/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+sudo echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$KUBE_VERSION_SHORT/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-#Install Kubeadm and Kubernetes Components using Kubeadm
-#References: https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
-#   Let IPTables to see the bridged traffic.
-
-sudo modprobe br_netfilter
-cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
-br_netfilter
-EOF
-
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-EOF
-
-sudo sysctl --system
-
-#   Update the apt package index and install packages needed to use the Kubernetes apt repository
-
-sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl
-
-#   Download the Google Cloud public signing key
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-
-#   Add the Kubernetes apt repository
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-#   Update apt package index, install kubelet, kubeadm and kubectl, and pin their version
-
-sudo apt-get update && sudo apt-get install -y kubelet=${KUBEVERSION} kubeadm=${KUBEVERSION} kubectl=${KUBEVERSION}
+# Install Kubelet, Kubeadm, and Kubectl
+sudo apt-get update && sudo apt-get install -y kubelet=${KUBEVERSION} kubeadm=${KUBEVERSION} kubectl=${KUBEVERSION} --allow-change-held-packages
 sudo apt-mark hold kubelet kubeadm kubectl
-sudo swapoff -a
-
+sudo systemctl enable --now kubelet
 
 #------------------------------------------------------------
